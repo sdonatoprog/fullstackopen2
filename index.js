3345bdd65436eac0d0665113a5de59e888f940e9
sdonatoprog <150184317+sdonatoprog@users.noreply.github.com>
2025-02-15 00:16:20 -0500

    phonebook back step 1 to 6

--- index.js
+++ index.js
@@ -2,3 +2,98 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+
+let persons =
+[
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+  const date = Date()
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    ${date.toString()}`
+  )
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id 
+  const person = persons.find((person) => person.id === id)
+  if (!person){
+    return response.status(404).end()
+  }
+  response.json(person)
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id 
+  persons = persons.filter((person) => person.id !== id)
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000) + 1
+}
+
+app.post("/api/persons", (request, response) => {
+  const id = generateId()
+  const body = request.body
+  
+  if (!body.name || !body.number)
+  {
+    return response.status(400).json(
+      {
+        error: "argument is missing"
+      }
+    )
+  }
+
+  const similarPerson = persons.find((person) => person.name == body.name)
+
+  if (similarPerson)
+  {
+    return response.status(400).json(
+      {
+        error: "name already exists"
+      }
+    )
+  }
+
+  const newPerson = 
+  {
+    id: String(id),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = [...persons, newPerson]
+
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
